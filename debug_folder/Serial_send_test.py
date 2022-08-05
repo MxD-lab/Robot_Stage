bd06c1b95ec1dcb24be0e2ce8fec98d1edfb0bfd
YuFunahashi <69149101+YuFunahashi@users.noreply.github.com>
2022-08-05 21:01:30 +0900

    Update Serial_send_test.py

--- debug_folder/Serial_send_test.py
+++ debug_folder/Serial_send_test.py
@@ -10,28 +10,28 @@ if __name__ == '__main__':
     ser = serial.Serial("COM3", 115200) 
     time.sleep(1)
     #ser.write(bytes("cariv_start;",'utf-8'))
-    ser.write("1".encode('utf-8'))
+    ser.write(bytes("1;",'utf-8'))
     while(True):
         if ser.inWaiting():
             str_buf = ser.readline().strip().decode('utf-8')
-            if str_buf == '2':
+            if str_buf == '1':
                 print("carivration end")
                 break
     ser.close()
 
-    while(True):
-        youser_input = input()
-        print(type(youser_input))
-        if youser_input == "1":
-            ser = serial.Serial("COM3", 115200) 
-            time.sleep(1)
-            ser.write(bytes("1",'utf-8'))
-            print("ここにはいる")
-            while(True):
-                if ser.inWaiting():
-                    str_buf = ser.readline().strip().decode('utf-8')
-                    if str_buf == "move_x_end":
-                        print("send_1_is_end")
-                        break
-            ser.close()
-        break
+    # while(True):
+    #     youser_input = input()
+    #     print(type(youser_input))
+    #     if youser_input == "1":
+    #         ser = serial.Serial("COM3", 115200) 
+    #         time.sleep(1)
+    #         ser.write(bytes("1",'utf-8'))
+    #         print("ここにはいる")
+    #         while(True):
+    #             if ser.inWaiting():
+    #                 str_buf = ser.readline().strip().decode('utf-8')
+    #                 if str_buf == "move_x_end":
+    #                     print("send_1_is_end")
+    #                     break
+    #         ser.close()
+    #     break
