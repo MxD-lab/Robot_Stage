7af5a0479cf1305e0b0bc7ad383547d1f2b9c369
tatsuya999 <ts2000kasa@gmail.com>
2024-11-15 17:21:21 +0900

    multiprocessingでの運用に変更

--- main_folder/measure.py
+++ main_folder/measure.py
@@ -7,11 +7,8 @@ from nidaqmx.constants import READ_ALL_AVAILABLE
 from nidaqmx import stream_readers as sr
 import serial
 import csv
-from threading import Thread, Event
-'''
-2022/06/09 プロセス間でのUDP通信できることを確認（ネットから拾ったコード動かしただけ）
-'''
-
+#from threading import Thread, Event
+import multiprocessing as mp
 
 def change_power(raw_data):
     #ロードセルから得られた電圧値をひずみ値にする
@@ -48,19 +45,13 @@ def change_power(raw_data):
 
 
 #Arduinoと接続、ロボットステージの制御用クラス
-class MoterControll():
+class MotorControll(mp.Process):
     ###ロードセルとの接続をコンストラクタで実行
-    def __init__(self,port="COM3",baudrate=115200):
-        self.serial = serial.Serial(port,baudrate)
-        #self.serial.setDTR(False) 現状効果なし
-        while True:
-          if self.serial.in_waiting > 0:
-            response = self.serial.readline().decode('utf-8', errors='ignore').strip()
-            print("Arduino:", response)
-            if response == "Setup":
-                break
-        time.sleep(1)
-        print("connect")
+    def __init__(self,queue,port="COM3",baudrate=115200):
+        self.queue = queue
+        self.port = port
+        self.baudrate = baudrate
+        super().__init__()
 
     ###シリアル通信経由のリセットでsetup()を再実行させる。
     def reset(self,port="COM3",baudrate=115200):
@@ -109,61 +100,85 @@ class MoterControll():
     def move_xyz(self,x,y,z,xspeed=1000,yspeed=1000,zspeed=1000):
         com = f"{xspeed},{x},{yspeed},{y},{zspeed},{z}\n"
         self.serial.write(com.encode())
-        #self.serial.flush()
+        self.serial.flush()
+        time.sleep(0.5)
     
     ####moveToForce~()呼び出し用メソッド
     def moveToForce(self,xspeed=0,yspeed=0,zspeed=0):
-       com = f"x={xspeed},y={yspeed},z={zspeed}"
+       com = f"x={xspeed},y={yspeed},z={zspeed}\n"
        self.serial.write(com.encode())
-       #self.serial.flush()
+       self.serial.flush()
+       time.sleep(0.5)
        
     def move_stop(self):
         self.serial.write(b"STOP\n")
-        #self.serial.flush()
+        self.serial.flush()
+
 
     def close(self):
         self.serial.close()
 
+    def run(self):
+        try:
+            self.serial = serial.Serial(self.port,self.baudrate)
+            #self.serial.setDTR(False) 現状効果なし
+            while True:
+                if self.serial.in_waiting > 0:
+                    response = self.serial.readline().decode('utf-8', errors='ignore').strip()
+                    print("Arduino:", response)
+                    if response == "Setup":
+                        break
+                time.sleep(1)
+                print("connect")    
+            self.calibration()
+            self.move_senpos()
+            power = self.queue.get()
+            print(f'receive power = {power}')
+        except KeyboardInterrupt:
+            self.close()
+            print('通信終了')
+
+
 #NiDaqとの接続、ロードセルと触覚センサの情報取得
-class DaqMeasure(MoterControll):
-    def __init__(self, device_name="Dev1", channels=6, sample_rate=1000, chunk_size=50):
+class DaqMeasure(mp.Process):
+    def __init__(self, queue, device_name="Dev1", channels=6, sample_rate=1000, chunk_size=50, filename="daq_data_continuous.csv"):
         self.device_name = device_name
         self.channels = channels
         self.sample_rate = sample_rate
         self.chunk_size = chunk_size  # 1回の読み取りで取得するサンプル数
         self.latest_data = None  # 最新の計測データを保持
-        #self.running = Event()  # 計測の開始/停止を管理するイベント        
+        self.filename = filename
+        self.queue = queue    
         super().__init__()
 
     #計測を行うメソッド呼び出し
-    def measurement(self, filename="daq_data_continuous.csv"):
-        def measure_vi():
-        # CSVファイルをオープン
-            with open(filename, mode='w', newline='') as file:
-                writer = csv.writer(file)
-                
-                # ヘッダー行の書き込み
-                header = [f"Channel {i+1}" for i in range(self.channels)]
-                writer.writerow(header)
-                
-                # NI-DAQmxでアナログ入力タスクを作成
-                with nidaqmx.Task() as task:
-                    # 各チャンネルをシングルエンドモードで設定
-                    for i in range(self.channels):
-                        task.ai_channels.add_ai_voltage_chan(
-                            f"{self.device_name}/ai{i}",
-                            terminal_config=TerminalConfiguration.RSE
-                        )
-                    
-                    # サンプリングレートと連続測定モードを設定
-                    task.timing.cfg_samp_clk_timing(
-                        self.sample_rate,
-                        sample_mode=AcquisitionType.CONTINUOUS
+    def run(self):
+    # CSVファイルをオープン
+        with open(self.filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            
+            # ヘッダー行の書き込み
+            header = [f"Channel {i+1}" for i in range(self.channels)]
+            writer.writerow(header)
+            
+            # NI-DAQmxでアナログ入力タスクを作成
+            with nidaqmx.Task() as task:
+                # 各チャンネルをシングルエンドモードで設定
+                for i in range(self.channels):
+                    task.ai_channels.add_ai_voltage_chan(
+                        f"{self.device_name}/ai{i}",
+                        terminal_config=TerminalConfiguration.RSE
                     )
+                
+                # サンプリングレートと連続測定モードを設定
+                task.timing.cfg_samp_clk_timing(
+                    self.sample_rate,
+                    sample_mode=AcquisitionType.CONTINUOUS
+                )
 
-                    print("測定を開始")
-
-                    while self.running.is_set():
+                print("測定を開始")
+                try:
+                    while True:
                         # データをチャンクサイズ分取得
                         self.data_chunk = np.array(task.read(number_of_samples_per_channel=self.chunk_size),dtype=float)
                         # データを行ごとにCSVに保存
@@ -171,6 +186,8 @@ class DaqMeasure(MoterControll):
                             self.data = [channel_data[i] for channel_data in self.data_chunk]
                             power = change_power(self.data[0:3])
                             self.latest_data = power
+                            #queueで共有
+                            self.queue.put(power)
                             #self.dataのロードセル部分を力変換かけたものに置き換え
                             for n in range(len(power)):
                                 self.data[n] = power[n,0]
@@ -178,24 +195,17 @@ class DaqMeasure(MoterControll):
                         #行の先頭を表示
                         force = f'x={self.data[0]},y={self.data[1]},z={self.data[2]}\n'
                         print(force)                       
-        #self.running.set()
-        Thread(target=measure_vi, daemon=True).start()
-
-    def get_latest_data(self):
-        #print(self.latest_data)
-        return self.latest_data
-
-    # def stop_measurement(self):
-    #     self.running.clear()
-
-
-
+                except KeyboardInterrupt:
+                    print('計測終了')
+                    return
 
 if __name__ == '__main__':
     ##この2つをどう動かすかスレッドにするかソケット通信にするか
-    loadread = DaqMeasure()
-    loadread.calibration()
-    loadread.move_senpos()
+    queue = mp.Queue()
+    loadread = DaqMeasure(queue)
+    motor = MotorControll(queue)
+    # loadread.calibration()
+    # loadread.move_senpos()
     #################################################################################
     ####以下計測動作サンプル###########################################################
         # loadread.moveToForce(xspeed,yspeed,zspeed)
@@ -217,95 +227,84 @@ if __name__ == '__main__':
     ####x方向に動かした後、y方向に動かす。
     ####どちらも動いた後高さをそのままにもとの位置に斜めにもどす。
     ####最後に開始位置にもどす
+
     try:
-        # 計測スレッドを開始
-        loadread.measurement("daq_data_test.csv")
-        #z軸方向動かす。
-        loadread.moveToForce(0,0,-20)
-        # メインスレッドでArduinoを制御
-        while True:
-            latest_data = loadread.get_latest_data()
-            if latest_data is not None:
-                z_force = latest_data[2][0]  #z方向の力を取得
-                print(type(z_force))
-                if z_force >= 3:
-                    print(f"Set force detected: z={z_force}")
-                    loadread.move_stop()
-                    break
-                else:
-                    print(f"Force within range: z={z_force}")
-            time.sleep(0.5)
-        time.sleep(1)
+        loadread.start()
+        motor.start()
+        loadread.join()
+        motor.join()
+        # loadread.moveToForce(0,0,10)
+        # while True:
+        #     latest_data = loadread.get_latest_data()
+        #     if latest_data is not None:
+        #         z_force = latest_data[2][0]  #z方向の力を取得
+        #         if z_force >= 5:
+        #             print(f"Set force detected: z={z_force}")
+        #             loadread.move_stop()
+        #             break
+        #         else:
+        #             print(f"Force: z={z_force}")
+        #     time.sleep(0.5)
 
-        loadread.moveToForce(0,0,-10)
-        while True:
-            latest_data = loadread.get_latest_data()
-            if latest_data is not None:
-                z_force = latest_data[2][0]  #z方向の力を取得
-                if z_force >= 5:
-                    print(f"Set force detected: z={z_force}")
-                    loadread.move_stop()
-                    break
-                else:
-                    print(f"Force: z={z_force}")
-            time.sleep(0.5)
+        # loadread.moveToForce(0,0,-10)
+        # while True:
+        #     loadread.res_read()
+        #     if loadread.res_read() != '0':
+        #         zpos = int(loadread.res_read())
+        #     latest_data = loadread.get_latest_data()
+        #     if latest_data is not None:
+        #         z_force = latest_data[2][0]  #z方向の力を取得
+        #         if z_force <= 3:
+        #             print(f"Set force detected: z={z_force}")
+        #             loadread.move_stop()
+        #             break
+        #         else:
+        #             print(f"Force: z={z_force}")
+        #     time.sleep(0.5)
 
-        loadread.moveToForce(0,0,10)
-        while True:
-            loadread.res_read()
-            if loadread.res_read() != '0':
-                zpos = int(loadread.res_read())
-            latest_data = loadread.get_latest_data()
-            if latest_data is not None:
-                z_force = latest_data[2][0]  #z方向の力を取得
-                if z_force <= 3:
-                    print(f"Set force detected: z={z_force}")
-                    loadread.move_stop()
-                    break
-                else:
-                    print(f"Force: z={z_force}")
-            time.sleep(0.5)
+        # loadread.moveToForce(10,0,0) #x方向を動かす
+        # while True:
+        #     #loadread.res_read()
+        #     latest_data = loadread.get_latest_data()
+        #     if latest_data is not None:
+        #         x_force = latest_data[0][0] #x方向の力を取得
+        #         if x_force >= 3:
+        #             print(f"Set force detected: x={x_force}")
+        #             loadread.move_stop()
+        #             break
+        #         else:
+        #             print(f"Force: x={x_force}")
+        #     time.sleep(0.5)
 
-        loadread.moveToForce(10,0,0) #x方向を動かす
-        while True:
-            #loadread.res_read()
-            latest_data = loadread.get_latest_data()
-            if latest_data is not None:
-                x_force = latest_data[0][0] #x方向の力を取得
-                if x_force >= 3:
-                    print(f"Set force detected: x={x_force}")
-                    loadread.move_stop()
-                    break
-                else:
-                    print(f"Force: x={x_force}")
-            time.sleep(0.5)
+        # loadread.moveToForce(0,10,0) #y方向を動かす
+        # while True:
+        #     #loadread.res_read()
+        #     latest_data = loadread.get_latest_data()
+        #     if latest_data is not None:
+        #         y_force = latest_data[1][0] #x方向の力を取得
+        #         if y_force >= 3:
+        #             print(f"Set force detected: y={y_force}")
+        #             loadread.move_stop()
+        #             break
+        #         else:
+        #             print(f"Force: y={y_force}")
+        #     time.sleep(0.5)
 
-        loadread.moveToForce(0,10,0) #y方向を動かす
-        while True:
-            #loadread.res_read()
-            latest_data = loadread.get_latest_data()
-            if latest_data is not None:
-                y_force = latest_data[1][0] #x方向の力を取得
-                if y_force >= 3:
-                    print(f"Set force detected: y={y_force}")
-                    loadread.move_stop()
-                    break
-                else:
-                    print(f"Force: y={y_force}")
-            time.sleep(0.5)
+        # loadread.move_xyz(25000,27000,zpos,100,100,0)
+        # while True:
+        #     loadread.res_read()
+        #     if loadread.res_read() == 'Done':
+        #         break
 
-        loadread.move_xyz(25000,27000,zpos,100,100,0)
-        while True:
-            loadread.res_read()
-            if loadread.res_read() == 'Done':
-                break
-
-        loadread.move_senpos()
+        # loadread.move_senpos()
 
     ########## CTRL+Cで終了        
     except KeyboardInterrupt:
-        loadread.move_stop()
-        #loadread.stop_measurement()
+        loadread.terminate()
+        motor.terminate()
+        loadread.join()
+        motor.join()        
         print("Program interrupted")
+    
     #######
     ##################################################################################
